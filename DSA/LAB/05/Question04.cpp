ff5259660d4841b4754322491a34382f4e9db665
talharusman <142867808+talharusman@users.noreply.github.com>
2024-10-04 20:00:24 +0500

    Update Question04.cpp

--- DSA/LAB/05/Question04.cpp
+++ DSA/LAB/05/Question04.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+using namespace std;
 
+int binarySearch(int arr[], int n, int target) {
+    int left = 0;
+    int right = n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1; 
+}
+
+int main() {
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int result = binarySearch(arr, n, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << std::endl;
+    } else {
+        cout << "Element not found in the array" << std::endl;
+    }
+
+    return 0;
+}
